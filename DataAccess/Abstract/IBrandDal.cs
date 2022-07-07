2c228f45ca052ac62a9ae941f46f24e03779a69d
BARIŞ IŞIK <20202305034@ogr.akdeniz.edu.tr>
2022-07-07 16:19:59 +0300

    07.07.22

--- DataAccess/Abstract/IBrandDal.cs
+++ DataAccess/Abstract/IBrandDal.cs
@@ -5,7 +5,7 @@ using System.Text;
 
 namespace DataAccess.Abstract
 {
-    public interface IBrandDal:IEntityRepository<Car>
+    public interface IBrandDal:IEntityRepository<Brand>
     {
     }
 }
